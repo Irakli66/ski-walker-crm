a571a8dc45b4afd22e98cf7ae54ddc30300f6808
irakli66 <xarshiladzeirakli@gmail.com>
2025-05-19 16:27:55 +0400

    update button size

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        xl: 'h-20 rounded-xl px-6 has-[>svg]:px-14',
+        xl: 'h-50 w-50 rounded-2xl has-[>svg]:px-14',
         icon: 'size-9',
       },
     },
