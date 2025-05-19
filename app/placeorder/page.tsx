c3960774a8408e0f4039576d61cb7eb2ea7df299
irakli66 <xarshiladzeirakli@gmail.com>
2025-05-19 16:24:43 +0400

    add: fonts

--- app/placeorder/page.tsx
+++ app/placeorder/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button';
+
 export default async function PlaceOrder() {
   return (
-    <div className="flex flex-col gap-4 items-center justify-center h-screen bg-primary text-background">
-      <h1 className="text-2xl font-bold">Place Order</h1>
-      <p className="text-gray-600">This is the place order page.</p>
+    <div className="flex flex-col gap-4 items-center justify-center h-screen bg-background text-foreground">
+      <h1 className="text-gray-600 text-md font-extralight">
+        განათავსე შეკვეთა
+      </h1>
+      <Button variant="default" size="xl" className="text-xl">
+        დაწყება
+      </Button>
     </div>
   );
 }
