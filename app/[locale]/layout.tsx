23ac84d3e815d2a0ec3c6ef8948bc7430c9c4963
irakli66 <xarshiladzeirakli@gmail.com>
2025-05-19 21:14:00 +0400

    no fonts

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextIntlClientProvider locale={locale}>
           {children}
