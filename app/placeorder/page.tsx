9db6f3dbc3304e1b15553ddaf3bcbfebcf541aa8
irakli66 <xarshiladzeirakli@gmail.com>
2025-05-19 15:45:50 +0400

    initial commit

---
+++ app/placeorder/page.tsx
@@ -0,0 +1,8 @@
+export default async function PlaceOrder() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-screen bg-primary text-background">
+      <h1 className="text-2xl font-bold">Place Order</h1>
+      <p className="text-gray-600">This is the place order page.</p>
+    </div>
+  );
+}
